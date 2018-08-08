3bca04be96fc0446a7b6ae6cfab11e68114f08b6
Angela Llano <angelallano96@gmail.com>
2018-08-08 16:55:26 +0000

    first commit

---
+++ js/myScript.js
@@ -0,0 +1,38 @@
+
+
+
+/*var Adult, Child, Senior,price, Quantity,Tickets,
+Total;
+
+Adult=36.95;
+Child=26.95;
+Senior=31.95;
+Quantity= [0,1,2,3,4,5,6,7,8,9,10];
+price=
+Total= Adult + Child + Senior;
+console.log("Adult")
+*/
+
+//var ticketType = [Adult, Senior, Child, Child2];
+
+//var tickePrice =[36.95, 31.95, 26.95, 0];
+
+//var Quantity = [0,1,2,3,4,5,6,7,8,9,10];
+
+//var Total = ticketType + ticketPrice + Quantity;
+// var [Adult=>13, Senior=>65, Child=
+
+/*  <h2>Using The id Attribute in JavaScript</h2>
+<p>JavaScript can access an element with a specified id by using the getElementById() method:</p>
+
+<td id="ticketType">Adult 13 and over</td>
+<button onclick="calculateTotal(ticketType *Quantity)">Calculate Total</button>
+
+<script>
+function calculateTotal(ticketPrice,Quantity) {
+return ticketType * Quantity
+
+    document.getElementById("ticketType").innerHTML = calculateTotal(ticketType,3);
+}
+</script>
+    */
