9b8e03b1dbf859a5e9ecb4e39d20db1a53de2faa
Angela Llano <angelallano96@gmail.com>
2018-08-13 13:29:07 +0000

    revised the code on all pages

--- js/myScript.js
+++ js/myScript.js
@@ -1,7 +1,7 @@
 
 
 
-/*var Adult, Child, Senior,price, Quantity,Tickets,
+/*var Adult, Child,Child2, Senior,price, Quantity,Tickets,
 Total;
 
 Adult=36.95;
@@ -35,4 +35,95 @@ return ticketType * Quantity
     document.getElementById("ticketType").innerHTML = calculateTotal(ticketType,3);
 }
 </script>
+   
+   /*
+    <div class="container">
+  <div class="row">
+    <div class="col-12 text-center">
+      <form method="post">
+          <label for="code">How old are you?</label>
+            <select name="cars" id="code">
+              <option value="0" selected="selected">Select Your Age...</option>
+              <option value="10">16 to 23</option>
+              <option value="20">24 to 65</option>
+              <option value="30">Over 66</option>
+                </select>
+          <label>&nbsp;</label><input name="submit" type="submit" id="button" value="GO" class="button" style="cursor:pointer"/>
+      </form>
+            <p id="message"></p>
+    </div>
+  </div>
+</div>
+
     */
+   
+   /*  
+    $(document).ready(function() {
+
+      // Initialize the plugin
+      $('#my_popup').popup({
+            transition: 'all 0.3s',
+            scrolllock: true // optional
+            });
+            
+
+    });
+    
+
+window.onload = function () {
+   document.getElementById('button').onclick = function() {
+     if(document.getElementById("code").value == '10')
+        location.href = "https://preview.c9users.io/angelall/group-project-phase2/pages/youth.html";
+     else if(document.getElementById("code").value == '20')
+     location.href = "https://preview.c9users.io/angelall/group-project-phase2/pages/adult.html";
+     else  if(document.getElementById("code").value == '30')
+     location.href = "https://preview.c9users.io/angelall/group-project-phase2/pages/senior.html";
+     else
+        //Create a div or p to show message
+        document.getElementById("message").innerHTML = 'Please Select Your Age Group First
+        ';
+     return false;//prevent form submission
+   }//.onclick
+}//window.onload
+//End Of Code.
+
+    
+    */
+    
+    
+    
+    /*
+    switch (expression) {
+    case value1:
+    run this code
+    break;
+    case value 2:
+    run this code;
+    break;
+    default:
+    run this code;
+    }
+    
+    $(document).ready(function(){
+    switch (expression )
+    {
+    
+    var adultQuantity = 36.95
+    
+    
+    
+        adult *
+    }
+    */
+    
+    
+    //animation
+    
+    
+    
+    //var img-fluid;
+    
+    
+    //$(document).ready(function() {
+        
+        
