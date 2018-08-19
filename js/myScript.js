f00d1bdaa0fa5df0419318fbc3b241b5c5d615bc
Angela Llano <angelallano96@gmail.com>
2018-08-19 06:42:31 +0000

    added animation

--- js/myScript.js
+++ js/myScript.js
@@ -4,4 +4,45 @@
       ticketType= new int [4]{36.95,26.95,31.95,0};
       quantity= new int [11]{0,1,2,3,4,5,6,7,8,9,10};
 
-      */  
+      */ 
+      
+     /* $(document).ready(function() { $('div').click(function)(){
+       $('div').effect('slide', {direction:'right'},1000);
+       document.getElementById(tigerStyle).innerHTML
+      });
+      
+      $(document).ready(function() { $('#tigerStyle').click(function(){
+       $(this).effect('slide', {direction:'right'},1000);});});
+       */
+       
+
+    
+      // $(document).ready(function(){
+      // $("#tigerStyle").click(function(){
+        //$("#tigerStyle").slideDown("slow");
+   // });
+//});
+
+  /*$(document).ready(function(){
+    $("tigerStyle").mouseover(function(){
+        $("tigerStyle").css("background-color", "yellow");
+    });
+    $("tigerStyle").mouseout(function(){
+        $("tigerStyle").css("background-color", "lightgray");
+    });
+});
+ */
+ 
+/* $(document).ready(function(){
+    $("#attraction").onloadstart(function(){
+        $("#attraction").slideDown(1000, function(){
+        
+        });
+    });
+    */
+
+      
+    
+    
+    
+    
