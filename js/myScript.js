8c86109e9bedfe724ccdf623b6c8540d7c96bdd9
Angela Llano <angelallano96@gmail.com>
2018-08-24 04:38:31 +0000

    changed attraction page image code

--- js/myScript.js
+++ js/myScript.js
@@ -47,7 +47,7 @@
 /*window.onload= function() {
     getElementByClassName()
 };
-*/
+
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
 
@@ -56,5 +56,4 @@
    });
   </script>  
     
-    
-    
+    */
