01ea20e714d7a0466c2cdfd2692548c8b7365028
Angela Llano <angelallano96@gmail.com>
2018-09-14 01:50:06 +0000

    ticket page

--- js/myScript.js
+++ js/myScript.js
@@ -5,16 +5,15 @@ function estimateTotal(event){
     event.preventDefault();
     /*console.log('You submitted the form.')*/
     
+    
     var ticketType=document.getElementById('adult');
+     
     
     if (ticketType.value===''){
         alert('Please select tickets.');
         ticketType.focus();
     }
-  var ticketAdult=document.getElementById ('adult').value;
-      ticketChild=document. getElementById ('child').value;
-      ticketSenior=document. getElementById ('senior').value;
-      ticketChild2=document. getElementById ('child2').value;  
+  var ticketAdult  =  parseInt(document. getElementById ('adult').value, 10)||0,  ticketChild  =  parseInt(document. getElementById ('child').value, 10)||0,  ticketSenior = parseInt(document. getElementById ('senior').value, 10)||0,  ticketChild2 = parseInt(document. getElementById ('child2').value,10)||0;  
       
       console.log(ticketAdult,ticketChild,ticketSenior,ticketChild2);
       
